9f56f222cf6822867263e929ca030e83441dac88
Shubham Jain <42095448+jainshubham11sep@users.noreply.github.com>
2022-03-06 21:36:16 +0530

    chnage in get type

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -6,14 +6,14 @@ const router=require('express').Router();
 // router for category
 
 router.post('/addcategory',categoryController.addCategory);
-router.get('/getcategory/:id',categoryController.getCategoryData);
+router.get('/getcategory/:user_id',categoryController.getCategoryData);
 router.put("/updatecategory",categoryController.updateCategoryData);
 router.delete("/deletecategory",categoryController.updateCategoryData);
 
 // router for sub category
 
 router.post('/addsubcategory',SubCategoryController.addSubCategory);
-router.get('/getsubcategory/:id',SubCategoryController.getSubCategoryData);
+router.get('/getsubcategory/:user_id',SubCategoryController.getSubCategoryData);
 router.put("/updatesubcategory",SubCategoryController.updateSubCategoryData);
 router.delete("/deletesubcategory",SubCategoryController.deleteSubCategoryData);
 
